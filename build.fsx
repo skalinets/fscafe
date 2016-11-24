344e5295b80868200e377c80244a5f7a4e191b4c
Serhiy Kalinets <kalinets@gmail.com>
2016-11-24 11:16:37 +0200

    fixes unit test runner

--- build.fsx
+++ build.fsx
@@ -1,29 +1,31 @@
 #r "packages/FAKE/tools/FakeLib.dll"
 
 open Fake
+open Fake.Testing.XUnit2
+
 let buildDir = "./build"
 let testsDir = "./tests"
 
-Target "Clean" (fun _ -> CleanDir buildDir)
+Target "Clean" (fun _ -> CleanDirs [buildDir; testsDir])
 
 Target "BuildApp" (fun _ -> 
         !! "src/**/*.fsproj"
-            -- "src/**/*.Tests.fsproj"
+            -- "src/**/*Tests.fsproj"
             |> MSBuildRelease buildDir "Build"
             |> Log "AppBuild-Output: "
 )
 
 Target "BuildTests" (fun _ -> 
         printfn "hello %A" "you"
-        !! "src/**/*.Tests.fsproj"
+        !! "src/**/*Tests.fsproj"
             |> MSBuildDebug testsDir "Build"
             |> Log "TestBuild-Output: "
 )
-let nunitRunnerPath = "./packages/NUnit.Runners/tools"
+let xunitRunnerPath = "./packages/xunit.runner.console/tools/xunit.console.exe"
 
 Target "RunUnitTests" (fun _ ->
-        !! (testsDir + "/*.Tests.dll")
-        |> NUnit (fun p -> { p with ToolPath = nunitRunnerPath})
+        !! (testsDir @@ "*Tests.dll")
+        |> xUnit2 (fun p -> { p with ToolPath = xunitRunnerPath})
 
         printfn "hello"
 )
